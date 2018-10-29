0de3225fe404893c606409155ca54eb5cdf4efe4
Samson Akanbi <samson.akanbi@outlook.com>
2018-10-29 18:54:08 +0100

    Added the BootStrap page.

--- Wrappixel Admin Lite/Controllers/HomeController.cs
+++ Wrappixel Admin Lite/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace Wrappixel_Admin_Lite.Controllers
             return View();
         }
 
+        public ActionResult BootstrapTables()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
