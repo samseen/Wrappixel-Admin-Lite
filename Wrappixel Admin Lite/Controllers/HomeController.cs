63abe7db5db38a19cad4fb1ca11d13a16aa6df48
Samson Akanbi <samson.akanbi@outlook.com>
2018-11-04 22:19:20 +0100

    Added the profile page.

--- Wrappixel Admin Lite/Controllers/HomeController.cs
+++ Wrappixel Admin Lite/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace Wrappixel_Admin_Lite.Controllers
             return View();
         }
 
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
