f58c888cb312d737f047be211d7475f2f866eb40
Samson Akanbi <samson.akanbi@outlook.com>
2018-11-06 23:24:53 +0100

    Added the fontawesome page

--- Wrappixel Admin Lite/Controllers/HomeController.cs
+++ Wrappixel Admin Lite/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace Wrappixel_Admin_Lite.Controllers
             return View();
         }
 
+        public ActionResult FontAwesome()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
